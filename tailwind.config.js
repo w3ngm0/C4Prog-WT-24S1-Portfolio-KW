3fe8ee9de1f60f82c0a7656811b78a2f3d33615c
w3ngm0 <waggmo@gmail.com>
2024-06-08 17:48:45 +0800

    feat(phase-5): Implementing Tailwind Styles, adding a header, navigation, footer, dashboard, main area, copyright page etc.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,11 @@
 module.exports = {
   content: [
       './index.html',
-      './phase-*/*.html',
-      './pages/**/*.html',
+      './phase-*/**/*.{html,js}',
+      './pages/**/*.{html,js}',
       './src/**/*.{html,js}',
-      './index.html',
-      './phase-1/**/index.html',
-      './phase-2/**/index.html',
-      './phase-3/**/index.html',
-      './phase-4/**/index.html',
+      './components/**/*.{html,js}'
+
   ],
   theme: {
     extend: {},
